0e8a222588dec120ed674facc164eda18b62dae4
nequalszero <nequalszero@gmail.com>
2017-06-21 01:22:47 -0700

    Implement media query for mobile responsiveness, fix calc in .todo-app class

--- src/components/todo/Footer.js
+++ src/components/todo/Footer.js
@@ -5,8 +5,8 @@ export const Footer = () => {
   return (
     <fieldset className="filters">
       <legend className="filters__title">Show:</legend>
-      <FooterRadio to='/'>All, </FooterRadio>
-      <FooterRadio to='/active'>Active, </FooterRadio>
+      <FooterRadio to='/'>All</FooterRadio>
+      <FooterRadio to='/active'>Active</FooterRadio>
       <FooterRadio to='/complete'>Completed</FooterRadio>
     </fieldset>
   )
